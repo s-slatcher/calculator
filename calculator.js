8328c946629862f7e45e1b282ead41e9052f0f0b
Selkie Slatcher <122629041+s-slatcher@users.noreply.github.com>
2023-02-10 17:58:36 -0500

    Completed

--- calculator.js
+++ calculator.js
@@ -2,82 +2,137 @@ let buttonsUpdate = document.querySelectorAll(".update");
 let buttonsOp = document.querySelectorAll(".op");
 let mainDisplay = document.querySelector("#main-display")
 let topDisplay = document.querySelector("#top-display");
+let equals = document.getElementById("equals");
+
 
 
 let opSelected = "";
 let updaterSelected = "";
 let sign = "";
+let currentDisplay = "";
+let topDisplayNumber = "";
+let expression = "";
+let expArr = [];
+let answer = 0;
+let cleared = true;
 
+equals.addEventListener('click', e => {
+    if (mainDisplay.textContent && topDisplay.textContent){
+        evaluate(e.target);
+    }
+})
 
 buttonsOp.forEach(e => {
     e.addEventListener('click', e => {
+        opSelected = e.target.id
         operateOnDisplay(e.target);
-        operatorHighlight(e.target);
     } , false);
 })
 
 buttonsUpdate.forEach(e => {
     e.addEventListener('click', e => {
+        cleared = false;
+        if (topDisplay.textContent && (topDisplay.textContent == topDisplayNumber)) allClear();
         updateDisplay(e.target);
     } , false);
 })
 
+function allClear(){
+    topDisplay.textContent = "";
+    mainDisplay.textContent = "";
+    sign = "";
+    expArr = [];
+    cleared = true;
+}
 
 function updateDisplay(button){
-        let currentDisplay = mainDisplay.textContent.replace("-","");
+        let tempDisplay = currentDisplay.replace("-","");
+        let isEmpty = !tempDisplay.length;
         if (mainDisplay.textContent.length > 12) return;
         if ([1,2,3,4,5,6,7,8,9,0].includes(+button.textContent)){
             updaterSelected = button.textContent;
         }
         switch (button.id){
             case "decimal":
-                if (currentDisplay.includes(".")) return;
-                if (currentDisplay === "") { 
-                    currentDisplay = "0."
+                if (tempDisplay.includes(".")) return;
+                if (isEmpty) { 
+                    tempDisplay = "0."
                 } else { 
                     updaterSelected = "."; }
                 break;
             case "change-sign":
+                if (isEmpty) return;
                 sign == "-" ? sign = "" : sign = "-";
                 break;
             case "clear":
-                currentDisplay = currentDisplay.slice(0,-1);
-                if (currentDisplay.length < 1){
-                    sign = "";
-                }
+                tempDisplay = tempDisplay.slice(0,-1);
+                if (isEmpty) sign = "";
                 break;
             case "all-clear":
-                currentDisplay = "";
-                topDisplay.textContent = "Calculator"
-                operatorHighlight();
-
+                tempDisplay = "";
+                allClear();
+                break;
         }
-        
-        mainDisplay.textContent = sign.concat(currentDisplay,updaterSelected);
+        mainDisplay.textContent = sign.concat(tempDisplay,updaterSelected);
+        currentDisplay = mainDisplay.textContent;
         updaterSelected = "";
+        if (!topDisplay.textContent && !mainDisplay.textContent) cleared = true; 
     } 
 
 function operateOnDisplay(op){
-    console.log(op.id);
-}
-
-
-function operatorHighlight(button = "clear"){
-    if (button == "clear"){
-        if (opSelected === "") { return }
-        document.getElementById(opSelected).classList.remove("selected");
+    sign = "";
+    if (cleared) {
         return;
-
     }
-    if (opSelected !== ""){
-        document.getElementById(opSelected).classList.remove("selected");
-    } 
-    if (opSelected !== button.id) {
-    opSelected = button.id;
-    button.classList.add("selected");    
-    } else {
-    opSelected = "";
+
+    if (topDisplay.textContent === "" && mainDisplay.textContent !== ""){
+        topDisplayNumber = currentDisplay;
+        topDisplay.textContent = `${topDisplayNumber} ${op.textContent}`;     //fill top display then clear out bottom
+        currentDisplay = "";
+        mainDisplay.textContent = currentDisplay;
+        return;
     }
     
+    if (currentDisplay == ""){
+        topDisplay.textContent = `${topDisplayNumber} ${op.textContent}`
+        return;
+    }
+
+    evaluate(op);
     
 }
+
+function evaluate(op){
+    opSelected = "";
+    expression = topDisplay.textContent.concat(" ",mainDisplay.textContent)
+    expArr = expression.split(" ")
+    let num1 = parseFloat(expArr[0]);
+    let num2 = parseFloat(expArr[2]);
+    switch (expArr[1]){
+        case "+": answer = num1 + num2;
+            break;
+        case "-": answer = num1 - num2;
+            break;
+        case "/":
+            if (expArr[2] == 0){
+                alert("Cannot divide by zero.")
+                allClear();
+                return;
+            }
+            answer = num1 / num2;
+            break;
+        case "x": answer = num1 * num2;
+            break;
+    }
+    if (op.textContent == "="){
+        topDisplay.textContent = answer.toString();
+        console.log(answer);
+    } else {
+        topDisplay.textContent = `${answer} ${op.textContent}`;
+    }
+    currentDisplay = "";
+    mainDisplay.textContent = currentDisplay;
+    topDisplayNumber = answer;
+    
+}
+
